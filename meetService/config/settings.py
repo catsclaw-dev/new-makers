7df9ebf47887887d970e6639862ce5c41af6069c
CatsClaw <vasil.v.maximov@gmail.com>
2026-06-24 18:40:50 +0300

    feat: added prod docker-compose.yml

--- meetService/config/settings.py
+++ meetService/config/settings.py
@@ -222,6 +222,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 if not DEBUG:
     STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
         "staticfiles": {
             "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
